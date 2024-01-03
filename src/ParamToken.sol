c31480b978053eb1e226d8e01aad14efd32a8da1
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2024-01-03 16:48:28 +0530

    rename

--- src/ParamToken.sol
+++ src/ParamToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol"; // Import Ownable
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract ParamToken is
